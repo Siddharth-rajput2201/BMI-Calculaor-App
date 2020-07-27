578db0db8027a7ee3370a9e01efa1957635822a4
Hush <rajputsiddharth18@gmail.com>
2020-07-27 15:47:25 +0530

    added color theme

---
+++ lib/BMICalculatorpage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BMICalculatorpage extends StatefulWidget {
+  @override
+  _BMICalculatorpageState createState() => _BMICalculatorpageState();
+}
+
+class _BMICalculatorpageState extends State<BMICalculatorpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI Calculator"),
+      ),
+      body: Center(
+        child: Text("TEXT"),
+      ),);
+  }
+}
